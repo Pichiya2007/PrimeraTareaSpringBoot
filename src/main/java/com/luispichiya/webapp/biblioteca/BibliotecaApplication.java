6e81558f8ddadc8c745cdd1f612395ab9bf3235a
Pichiya2007 <Pichiya2007>
2024-09-12 01:07:41 -0600

    Base para JavaFX

--- src/main/java/com/luispichiya/webapp/biblioteca/BibliotecaApplication.java
+++ src/main/java/com/luispichiya/webapp/biblioteca/BibliotecaApplication.java
@@ -3,10 +3,16 @@ package com.luispichiya.webapp.biblioteca;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.luispichiya.webapp.biblioteca.system.Main;
+
+import javafx.application.Application;
+
 @SpringBootApplication
 public class BibliotecaApplication {
 
 	public static void main(String[] args) {
+
+		Application.launch(Main.class, args);
 		SpringApplication.run(BibliotecaApplication.class, args);
 	}
 
--- src/main/java/com/luispichiya/webapp/biblioteca/controller/CategoriaController.java
+++ src/main/java/com/luispichiya/webapp/biblioteca/controller/CategoriaController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ import com.luispichiya.webapp.biblioteca.model.Categoria;
 @Controller
 @RestController
 @RequestMapping(value = "")
+@CrossOrigin(origins = "http://localhost:5173")
 public class CategoriaController {
 
     @Autowired
